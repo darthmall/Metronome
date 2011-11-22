aec14d3b5d5f76eb792b1b33dca7988062530b9d
Evan Sheehan <wallace.sheehan@gmail.com>
2011-11-21 20:24:17 -0500

    Initial Commit

---
+++ metronome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  metronome
+//
+//  Created by Evan Sheehan on 11/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ metronome/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  metronome
+//
+//  Created by Evan Sheehan on 11/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
